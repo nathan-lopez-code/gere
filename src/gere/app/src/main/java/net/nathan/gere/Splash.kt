d18d1312807d3adff70feabefe21d6231bff3dad
kazaku lopez nathan <19k212@esisalama.org>
2022-04-15 14:03:38 +0200

    start project

---
+++ src/gere/app/src/main/java/net/nathan/gere/Splash.kt
@@ -0,0 +1,22 @@
+package net.nathan.gere
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash : AppCompatActivity() {
+
+    private val MYTIME : Long = 3500
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, MYTIME)
+    }
+}
