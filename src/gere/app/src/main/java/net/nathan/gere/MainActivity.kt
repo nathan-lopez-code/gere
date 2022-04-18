a7f97bf1495e765ef30764e54d9c4e2c17394e08
kazaku lopez nathan <19k212@esisalama.org>
2022-04-18 12:08:07 +0200

    basique UI for main activity

--- src/gere/app/src/main/java/net/nathan/gere/MainActivity.kt
+++ src/gere/app/src/main/java/net/nathan/gere/MainActivity.kt
@@ -2,10 +2,18 @@ package net.nathan.gere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import net.nathan.gere.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
