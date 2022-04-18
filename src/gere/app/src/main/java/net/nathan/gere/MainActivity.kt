eb89e3c7ac43cbb56f6bc06f3605f3b0513e375a
kazaku lopez nathan <19k212@esisalama.org>
2022-04-18 12:36:43 +0200

    basique UI for main activity

--- src/gere/app/src/main/java/net/nathan/gere/MainActivity.kt
+++ src/gere/app/src/main/java/net/nathan/gere/MainActivity.kt
@@ -1,9 +1,10 @@
 package net.nathan.gere
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import net.nathan.gere.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +15,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // creat an calendar instance
+
+        val calendar = Calendar.getInstance()
+
+        val date = DatePickerDialog.OnDateSetListener{
+            view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(calendar)
+        }
+
+
+    }
+
+    private fun updateLabel(calendar: Calendar) {
+        val format = "dd-MM--yyyy"
 
     }
 }
